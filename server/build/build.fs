23b317368d71cfd35e0678a7b23ead2d05c4e4b8
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-05-05 17:35:30 +0900

    chore(server): setup fake

---
+++ server/build/build.fs
@@ -0,0 +1,25 @@
+﻿open Fake.Core
+
+module Command =
+    [<Literal>]
+    let Default = "Default"
+
+let initTargets () =
+    Target.create Command.Default (fun _ -> printfn "hello from FAKE!")
+
+[<EntryPoint>]
+let main args =
+    Context.FakeExecutionContext.Create false "build.fsx" []
+    |> Context.RuntimeContext.Fake
+    |> Context.setExecutionContext
+
+    initTargets ()
+
+    try
+        match args with
+        | [| target |] -> Target.runOrDefault target
+        | _ -> Target.runOrDefault Command.Default
+    with _ ->
+        ()
+
+    0
